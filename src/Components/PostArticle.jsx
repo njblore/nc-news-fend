4797337860f0d4da4c076825c24c25dd87ab249f
njblore <njblore@outlook.com>
2019-04-25 12:01:07 +0100

    radio buttons working on post article form

--- src/Components/PostArticle.jsx
+++ src/Components/PostArticle.jsx
@@ -2,38 +2,73 @@ import React, { Component } from 'react';
 import { fetchTopics } from '../api';
 
 class PostArticle extends Component {
-  state = { topics: null };
+  state = {
+    topics: null,
+    title: '',
+    body: '',
+    topic: '',
+    currentlyChecked: null,
+  };
   render() {
     return (
       <div className="post-article">
         <button onClick={this.props.handlePostArticleClick}>Cancel</button>
         <form className="article-form">
           Title:
-          <input onChange={this.handleTyping} />
+          <input
+            onChange={e => this.handleTyping(e.target.value, 'title')}
+            value={this.state.title}
+          />
           Topic:
           {this.state.topics &&
             this.state.topics.map(topic => {
               return (
                 <div className="checkbox" key={topic.slug}>
                   <label htmlFor={topic.slug}>{topic.slug}:</label>
-                  <input type="checkbox" value={topic.slug} name={topic.slug} />
+                  <input
+                    type="radio"
+                    value={topic.slug}
+                    name={topic.slug}
+                    onChange={this.handleRadioClick}
+                    checked={this.state.currentlyChecked === topic.slug}
+                  />
                 </div>
               );
             })}
           <div className="checkbox">
-            <label htmlFor="other">Other:</label>
-            <input type="text" name="other" />
+            <label htmlFor="other">another one:</label>
+            <input
+              type="radio"
+              value="other"
+              name="other"
+              onChange={this.handleRadioClick}
+              checked={this.state.currentlyChecked === 'other'}
+            />
+            {this.state.currentlyChecked === 'other' && (
+              <input type="text" name="other" />
+            )}
           </div>
           Body:
-          <input />
+          <input
+            onChange={e => this.handleTyping(e.target.value, 'body')}
+            value={this.state.body}
+          />
           <button>Post Article</button>
         </form>
+        <p>hello</p>
       </div>
     );
   }
 
-  handleTyping = e => {
-    console.log(e.target);
+  handleTyping = (value, input) => {
+    console.log(value);
+    this.setState(() => {
+      return input === 'title' ? { title: value } : { body: value };
+    });
+  };
+
+  handleRadioClick = e => {
+    this.setState({ currentlyChecked: e.target.value });
   };
 
   componentDidMount() {
